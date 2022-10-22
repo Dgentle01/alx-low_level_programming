9bccd53ea6c731a4a9f1ed25a49540fd456d0847
Seyi <oluseyisennuga015@gmail.com>
2022-10-22 06:15:41 -0700

    prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative"
+ * Return-: Always 0 (success)
+ */
+int main(void)
+{
+char c;
+
+for (c = '0'; c <= '0'; c++)
+{
+	printf(c);
+}
+putchar('\n');
+return (0);
+}
